1fe6a06577de37db81a34add5c717efceb59e3c0
tiimsterboer <tim.beemsterboer@gmail.com>
2018-01-20 12:37:53 -0800

    Initial Commit

---
+++ TV Triage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TV Triage
+//
+//  Created by Tim Beemsterboer on 1/20/18.
+//  Copyright © 2018 Tim Beemsterboer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
